5330e8811d11a2e25d9ce5b4c06982571c78d0d0
flaura@izipay.pe <flaura@izipay.pe>
2025-03-24 16:25:18 -0500

    feat: dependency library

--- src/main/java/com/data/examen/controllers/TransactionController.java
+++ src/main/java/com/data/examen/controllers/TransactionController.java
@@ -6,7 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import pe.migraciones.common.dto.ExampleRequestDTO;
+import pe.migraciones.common.dto.ExampleResponseDTO;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.Optional;
 
 @RestController
@@ -34,4 +38,21 @@ public class TransactionController {
         Transaction transactionDb = transactionService.saveTransaction(transaction);
         return ResponseEntity.status(HttpStatus.CREATED).body(transactionDb);
     }
+
+    @PostMapping("/library")
+    public ResponseEntity<?> exampleLibrary(@RequestBody ExampleRequestDTO request){
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(ExampleResponseDTO.builder()
+                .fullName(request.getName()+" "+request.getLastName())
+                        .age(calcularEdad(request.getBirthdate()))
+                .build());
+    }
+
+    public static int calcularEdad(LocalDateTime fechaNacimiento) {
+        LocalDateTime ahora = LocalDateTime.now();
+        return (int) ChronoUnit.YEARS.between(fechaNacimiento, ahora);
+    }
+
+
+
 }
